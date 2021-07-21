90d351305c879820fa90df86f58e657065d9b5fb
mgulbazilyas <mgulbazilyas@gmail.com>
2021-07-21 05:29:05 +0500

    Adding multiple instances

--- start_script.bat
+++ start_script.bat
@@ -1,11 +1,12 @@
 @echo off
 
-set /A timeout_val=450
+set /A timeout_val=900
 start "" "C:\Program Files\BlueStacks_nxt\HD-Player.exe" --instance Nougat32 --cmd launchApp --package "com.grindrapp.android"
-
 set list=1 2 3 4
 (for %%a in (%list%) do ( 
    echo %%a 
+timeout %timeout_val% > NUL
+
 start "" "C:\Program Files\BlueStacks_nxt\HD-Player.exe" --instance Nougat32_%%a --cmd launchApp --package "com.grindrapp.android"
 )
 
