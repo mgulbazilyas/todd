61761270569262a20ff04222fcb1558684975008
mgulbazilyas <mgulbazilyas@gmail.com>
2021-07-21 05:37:05 +0500

    Adding multiple instances

--- start_script.bat
+++ start_script.bat
@@ -1,13 +1,27 @@
 @echo off
 
 set /A timeout_val=900
+set /A stop_timeout_val=440
 start "" "C:\Program Files\BlueStacks_nxt\HD-Player.exe" --instance Nougat32 --cmd launchApp --package "com.grindrapp.android"
 set list=1 2 3 4
 (for %%a in (%list%) do ( 
    echo %%a 
-timeout %timeout_val% > NUL
+    timeout %stop_timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout %stop_timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout 20 > NUL
 
 start "" "C:\Program Files\BlueStacks_nxt\HD-Player.exe" --instance Nougat32_%%a --cmd launchApp --package "com.grindrapp.android"
 )
 
 )
+    timeout %stop_timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout %stop_timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout 20 > NUL
--- stop_script.bat
+++ stop_script.bat
@@ -1,17 +1,6 @@
 @echo off
 
-set /A timeout_val=440
+set /A stop_timeout_val=440
 
 
-set list=1 2 3 4 5
-(for %%a in (%list%) do ( 
-   echo %%a 
 
-    timeout %timeout_val% > NUL
-    taskkill /IM BlueStacks.exe /F
-    taskkill /IM HD-Player.exe /F
-    timeout %timeout_val% > NUL
-    taskkill /IM BlueStacks.exe /F
-    taskkill /IM HD-Player.exe /F
-    timeout 20 > NUL
-))
