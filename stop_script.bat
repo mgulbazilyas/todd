2558893c9f1503142891d69ea558e09fa2921926
mgulbazilyas <mgulbazilyas@gmail.com>
2021-07-21 05:25:42 +0500

    Adding multiple instances

--- stop_script.bat
+++ stop_script.bat
@@ -1,6 +1,17 @@
-timeout 750 > NUL
-taskkill /IM BlueStacks.exe /F
-taskkill /IM HD-Player.exe /F
-timeout 750 > NUL
-taskkill /IM BlueStacks.exe /F
-taskkill /IM HD-Player.exe /F
+@echo off
+
+set /A timeout_val=440
+
+
+set list=1 2 3 4 5
+(for %%a in (%list%) do ( 
+   echo %%a 
+
+    timeout %timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout %timeout_val% > NUL
+    taskkill /IM BlueStacks.exe /F
+    taskkill /IM HD-Player.exe /F
+    timeout 20 > NUL
+))
