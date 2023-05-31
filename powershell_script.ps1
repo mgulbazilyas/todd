8bfbc76adcf1080c14c505e1ec3b842b150de3c2
gulbaz <mgulbazilyas@gmail.com>
2023-05-31 20:58:51 +0500

    update for 65 seconds and userid

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -11,9 +11,21 @@ timeout /t 10
 Set-Location todd
 git stash
 git pull
+
+$filePath = "Grinder start stop.xml"
+
+# Get the current user's ID
+$currentUserID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+
+# Read the XML file content
+$xmlContent = Get-Content -Path $filePath -Raw
+
+# Replace the placeholder with the current user's ID
+$updatedContent = $xmlContent -replace "USERIDTOREPLACE", $currentUserID
+
+# Write the updated content back to the XML file
+$updatedContent | Set-Content -Path $filePath
+
 .\installationPart2.ps1
 
-
 Start-ScheduledTask -TaskName "grinder start stop app"
-
-
--- temp.ps1
+++ temp.ps1
@@ -1,14 +1,14 @@
 $instances = Get-ChildItem -Directory "C:\ProgramData\BlueStacks_nxt\Engine" -Exclude "Manager","Promotions","UserData"
-$stop_timeout_val = 40;
+# $stop_timeout_val = 40;
 Write-Output $instance
 while (1) {
     For ($i=0; $i -lt $instances.Length; $i++) {
         $instance = $instances[$i].BaseName
         Start-Process -FilePath "C:\Program Files\BlueStacks_nxt\HD-Player.exe" -ArgumentList "--instance $instance", "--cmd launchApp", '--package "com.grindrapp.android"'
-        timeout /t $stop_timeout_val
+        timeout /t 65
         # taskkill /IM BlueStacks.exe /F
         taskkill /IM HD-Player.exe /F
-        timeout /t $stop_timeout_val
+        timeout /t 15
         # taskkill /IM BlueStacks.exe /F
         taskkill /IM HD-Player.exe /F
         }
